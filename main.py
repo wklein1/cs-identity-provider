f90c20f4b8dfb4461c73a7d4576735b0ed686784
wklein1 <Witali@>
2022-11-01 16:58:33 +0100

    #9 add token_validation_model, add validate enpoint description

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
+from datetime import datetime,timedelta
+from modules.jwt.jwt_module import JwtEncoder
+import jwt
 import deta
 
 app = FastAPI()
 
 PROJECT_KEY = config("PROJECT_KEY")
+JWT_SECRET = config("JWT_SECRET")
+JWT_ALGORITHM = config("JWT_ALGORITHM")
 
 deta = deta.Deta(PROJECT_KEY)
 usersDB = deta.Base("users")
@@ -23,3 +28,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"])
+
+
+@app.post(
+    "/validate",
+    description="Validates the token from the request body.",
+    response_description="Returns key value pair 'is_valid:boolean'.",
+)
+def validate_token(token: str):
+    pass
