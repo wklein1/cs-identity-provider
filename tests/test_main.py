08b3c490ef1bc5bbf29ff9e75545e7c25231156f
wklein1 <Witali@>
2022-11-02 10:13:03 +0100

    #26 fix delete user endpoint invalid password test

--- tests/test_main.py
+++ tests/test_main.py
@@ -263,22 +263,9 @@ def test_delete_user_endpoint():
 def test_delete_user_endpoint_invalid_password():
     #ARRANGE
     client = TestClient(app)
-    JWT_SECRET = config("JWT_SECRET")
-    jwt_aud="kbe-aw2022-frontend.netlify.app"
-    jwt_iss="cs-identity-provider.deta.dev"
-    jwt_encoder = JwtEncoder(JWT_SECRET, "HS256")
-    test_user = {
-        "first_name":"test",
-        "last_name":"test",
-        "user_name":"test_usr2",
-        "email":"test@test.com",
-        "password":"testtesttest4"
-    }
-    new_user = client.post("/users",json=test_user)
-    new_user = new_user.json()
-    new_user_id = jwt_encoder.decode_jwt(new_user["token"],audience=jwt_aud,issuer=jwt_iss)["userId"]
+    TEST_USER_ID = config("TEST_USER_ID")
     del_user = {
-        "userId":new_user_id
+        "userId":TEST_USER_ID
     }
     expected_error = {
         "detail":"Invalid password"
